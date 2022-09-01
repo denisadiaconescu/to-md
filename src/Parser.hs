ed1c0793478350377af0f288a060f1794d461f58
Evie Ciobanu <me@eevie.ro>
2022-09-01 21:37:19 +0300

    use lexemes/symbols

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,33 +1,39 @@
-module Parser where
+module Parser (ToMd (..), parser) where
 
 import Data.Void
 import Text.Megaparsec
 import Text.Megaparsec.Char
-import Debug.Trace
 import qualified Text.Megaparsec.Char.Lexer as L
 
 newtype ToMd = ToMd
   { inputFile :: String
   }
-  deriving Show
+  deriving (Show)
 
 type Parser = Parsec Void String
 
 parser :: Parser ToMd
 parser = do
   ignoreComments
-  string "from ["
-  file <- many (noneOf [']'])
-  char ']'
-  ignoreComments
-  string "make md"
-  ignoreComments
+  keyword "from"
+  file <- around '[' ']'
+  keyword "make md"
   pure $ ToMd {inputFile = file}
 
+-------------------------------------------------------------------------------
+-- Helpers
 
 ignoreComments :: Parser ()
-ignoreComments = L.space 
+ignoreComments =
+  L.space
     space1
     (L.skipLineComment "#")
     empty
 
+keyword :: String -> Parser ()
+keyword key = do
+  L.symbol ignoreComments key
+  pure ()
+
+around :: Char -> Char -> Parser String
+around start end = between (keyword [start]) (keyword [end]) (many $ noneOf [end])
